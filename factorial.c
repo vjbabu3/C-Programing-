79fa37dc64eedc04700217537794e461204312d9
Vijay Babu <vb9477998@gmail.com>
2026-03-16 19:35:00 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 int main() {
     int num, i;
     long long factorial = 1;
-
     printf("Enter a number: ");
     scanf("%d", &num);
-
     for (i = 1; i <= num; i++) {
         factorial *= i;
     }
-
     printf("Factorial = %lld\n", factorial);
-
     return 0;
 }
